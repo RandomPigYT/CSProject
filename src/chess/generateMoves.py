1f96d9d843c2a844d7589809d7022fd68d409cc5
RandomPigYT <vinecompilation864@gmail.com>
2022-11-01 19:45:52 +0530

    added pawn movement logic

--- src/chess/generateMoves.py
+++ src/chess/generateMoves.py
@@ -1,10 +1,69 @@
 from chess import globals as g
+from chess import util as u
 
 # N, S, E, W, NE, NW, SE, SW
 directions = [8, -8, 1, -1, 9, 7, -7, -9]
 
 
-def sliding():
+def pawns() -> list:
+
+    piece = g.heldPiece[0] & 0b00111
+    pos = g.heldPiece[1]
+    colour = g.heldPiece[0] & 0b11000
+
+    possibleMoves = []
+
+    inInitialRank = (
+        lambda colour, pos: True
+        if (colour == g.Piece.white and u.indexToCoords(pos)[1] == 1)
+        or (colour == g.Piece.black and u.indexToCoords(pos)[1] == 6)
+        else False
+    )
+
+    inFinalRank = (
+        lambda colour, pos: True
+        if (colour == g.Piece.white and u.indexToCoords(pos)[1] == 7)
+        or (colour == g.Piece.black and u.indexToCoords(pos)[1] == 0)
+        else False
+    )
+
+    colourMod = lambda colour: 1 if (colour == g.Piece.white) else -1
+
+    # check for double pawn push
+    if (
+        inInitialRank(colour, pos)
+        and g.board[pos + (colourMod(colour) * (2 * directions[0]))] == 0
+    ):
+        possibleMoves.append(pos + (colourMod(colour) * (2 * directions[0])))
+
+    # check for single pawn push
+    if (
+        not inFinalRank(colour, pos)
+        and g.board[pos + (colourMod(colour) * directions[0])] == 0
+    ):
+        possibleMoves.append(pos + (colourMod(colour) * directions[0]))
+
+    # TODO: check for en passant
+
+    # check captures
+
+    if (
+        g.board[pos + (colourMod(colour) * directions[4])] & 0b11000 != colour
+        and g.board[pos + (colourMod(colour) * directions[4])] != 0
+    ):
+        possibleMoves.append(pos + (colourMod(colour) * directions[4]))
+
+    if (
+        g.board[pos + (colourMod(colour) * directions[5])] & 0b11000 != colour
+        and g.board[pos + (colourMod(colour) * directions[5])] != 0
+    ):
+        possibleMoves.append(pos + (colourMod(colour) * directions[5]))
+
+    return possibleMoves
+
+
+def sliding() -> list:
+
     piece = g.heldPiece[0] & 0b00111
     pos = g.heldPiece[1]
     colour = g.heldPiece[0] & 0b11000
@@ -29,11 +88,11 @@ def sliding():
 
             if g.board[temp] == 0:
                 possibleMoves.append(temp)
-            
+
             elif g.board[temp] & 0b11000 != colour:
-                possibleMoves.append(temp);
+                possibleMoves.append(temp)
                 break
-            
+
             elif g.board[temp] & 0b11000 == colour:
                 possibleMoves.append(temp - directions[i + offset[0]])
                 break
@@ -43,6 +102,12 @@ def sliding():
 
 def generateMoves() -> list:
 
-    moves = sliding()
+    moves = list(range(len(g.board)))
+
+    if g.heldPiece[0] & 0b00111 >= g.Piece.Bishop:
+        moves = sliding()
+
+    if g.heldPiece[0] & 0b00111 == g.Piece.Pawn:
+        moves = pawns()
 
     return moves
--- src/chess/util.py
+++ src/chess/util.py
@@ -1,2 +1,6 @@
 def coordsToIndex(rank, file):
     return rank * 8 + file
+
+
+def indexToCoords(index):
+    return (index % 8, int(index / 8))
